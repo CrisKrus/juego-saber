9c044bf1948864514cceaa5a57dadb98db3cf7ed
CrisKrus <cristian.suarez.ulpgc@gmail.com>
2018-05-22 11:19:26 +0100

    RED: given 4 should return IV

--- __tests__/roman-numerals.spec.js
+++ __tests__/roman-numerals.spec.js
@@ -6,5 +6,6 @@ describe('Roman numerals', function () {
         expect(romanNumerals.fromNumber(1)).toBe('I');
         expect(romanNumerals.fromNumber(2)).toBe('II');
         expect(romanNumerals.fromNumber(3)).toBe('III');
+        expect(romanNumerals.fromNumber(4)).toBe('IV');
     });
 });
