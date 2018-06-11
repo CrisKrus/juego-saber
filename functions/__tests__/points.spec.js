9543b248db6e48cf489c8270d2c98464f7e6d5ba
CrisKrus <cristian.suarez.ulpgc@gmail.com>
2018-06-11 12:51:57 +0100

    Solve test problems

--- functions/__tests__/points.spec.js
+++ functions/__tests__/points.spec.js
@@ -11,8 +11,7 @@ describe("points from UI", function () {
         pageObject;
 
     beforeEach(function () {
-        // document.body.innerHTML = pug.compileFile('./views/main.pug', null)();
-        document.body.innerHTML = pug.compileFile('../views/main.pug', null)();
+        document.body.innerHTML = pug.compileFile('./views/main.pug', null)();
         app = createGame(
             createQuestionNavigator,
             saberganerScoreManager,
