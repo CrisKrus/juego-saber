006fe35d14098f1b5dfafecbb0adf5f4d26a553a
CrisKrus <cristian.suarez.ulpgc@gmail.com>
2018-06-12 15:56:29 +0100

    RED statistics should start with zero correct answers

---
+++ functions/__tests__/statistics.spec.js
@@ -0,0 +1,15 @@
+describe('statistics', () => {
+    let stats;
+
+    beforeEach(() => {
+        stats = statistics();
+    });
+
+    it('should start with zero correct answers', function () {
+        expect(stats.correctAnswers()).toBe(0);
+    });
+});
+
+export default function statistics(){
+
+};
